f955c191d1759e8e9163e88df447e95f4b411cd0
domi22 <491431567@qq.com>
2020-08-02 11:15:35 +0800

    init client

---
+++ src/main/java/spring/boot/template/SpringBootTemplateApp.java
@@ -0,0 +1,12 @@
+package spring.boot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTemplateApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTemplateApp.class);
+    }
+}
